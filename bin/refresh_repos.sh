f2003864c4f3480e659d8e1cb29718f729f2247d
Irina Kaprizkina <irinakaprizkina@clearchannel.com>
2014-01-06 15:15:33 -0500

    use qac1-exclusive branch for staging

--- bin/refresh_repos.sh
+++ bin/refresh_repos.sh
@@ -4,22 +4,32 @@ cd ~
 
 for i in $(cat ~/bin/repo_list)
 do
+	organization=${i%%\/*}
+	repo=${i##*\/}
+
+	if [[ $organization == 'OPS-QAC1' && $repo == 'iheart-chef' ]]
+	then
+		master='qac1-exclusive'
+	else
+		master='master'
+	fi
+
 	if [[ -d $i ]]
 	then
 		echo "Refreshing $i"
 		cd $i
-		git checkout master
+		git checkout $master
 		git pull
 	else
 		echo "Cloning $i"
-		organization=${i%%\/*}
-		repo=${i##*\/}
 		echo $organization
 		echo $repo
 
 		cd $organization
 		echo "git clone  \"git@github.ihrint.com:${organization}/${repo}.git\""
 		git clone "git@github.ihrint.com:${organization}/${repo}.git"
+		git checkout $master
+		git pull
 	fi
 	cd  ~
 done
