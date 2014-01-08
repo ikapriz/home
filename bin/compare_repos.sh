6e772a6b295965937c93d30321e59fab015f1288
Irina Kaprizkina <irinakaprizkina@clearchannel.com>
2014-01-08 14:02:33 -0500

    t

--- bin/compare_repos.sh
+++ bin/compare_repos.sh
@@ -5,3 +5,5 @@ cd ~
 diff -r -x '.git' OPS-QAC1/ingestion OPS/ingestion
 diff -r -x '.git' OPS-QAC1/postgresql OPS/postgresql
 diff -r -x '.git' OPS-QAC1/authorization OPS/authorization
+diff -r -x '.git' DB-OPS-QAC1/mongodba DB-OPS/mongodba
+diff -r -x '.git' DB-OPS-QAC1/postgresdba DB-OPS/postgresdba
