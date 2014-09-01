9b3f6f495f60f4d448677a56da30b02e75efabb7
Irina Kaprizkina <irinakaprizkina@clearchannel.com>
2014-09-01 09:16:04 -0500

    added first version of chef backup

---
+++ bin/run_chef_dump.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# This directories are required for the script
+MACHINE=`hostname -a`
+BASENAME=`basename $0`
+sdate=`date +'%Y%m%d_%H%M%S'`
+NAGIOS_HOST=$(hostname)
+NAGIOS_HOST=${NAGIOS_HOST%%.*}
+NAGIOS_SERVICE='Mongodb-Backups'
+
+function usage {
+        echo $1
+
+        echo "Usage: $0 -h <host_ip_or_name> -p <port> -d <backup_dir>[-s <days>]"
+        echo "    <host_ip_or_name> - host ip or name for mongos instancei or one of the replica server"
+        echo "    <port> - port number  listenserver on (default port number is 27017)"
+        echo "    <days> - Remove backups older than <days>"
+        echo "    <backup_dir> - Use this directory for backups"
+	echo ""
+        echo "Usage: $0 -f <config_file>"
+	echo "  config file format:"
+	echo "		host=<host_ip_or_name>"
+	echo "		port=<port>"
+	echo "		days=<days>"
+        exit $2
+}
+
+# set defaults
+
+host=''
+port=''
+days=60
+dir='/data/db/backups'
+
+# Parse the parameters
+while getopts ":h:p:f:s:d:" opt; do
+  case $opt in
+    h)
+      host_par=$OPTARG
+      ;;
+    p)
+      port_par=$OPTARG
+      ;;
+    s)
+      days=$OPTARG
+      ;;
+    d)
+      dir=$OPTARG
+      ;;
+    f)
+      conffile=$OPTARG
+      conffile_set=1
+      ;;
+    \?)
+      usage "Invalid option: -$OPTARG"  1
+      ;;
+  esac
+done
+
+LOGDIR=`mktemp -d /var/log/chefdba/mongo_${BASENAME}_XXXXXXX`
+LOCKFILE="/var/log/mongo/${BASENAME}_${port}.lock"
+OUT="$LOGDIR/out"
+ERR="$LOGDIR/err"
+
+trap "rm -rf $LOGDIR $LOCKFILE" EXIT
+LOG="/var/log/chefdba/${BASENAME}_${sdate}_${port}.log"
+
+
