d8ee81f134c809adcaea1a2978e269064c9fdfc4
Irina Kaprizkina <irinakaprizkina@clearchannel.com>
2014-09-30 22:29:23 -0400

    Full list

--- bin/collectsysctl.bash
+++ bin/collectsysctl.bash
@@ -1,4 +1,12 @@
 for i in $(seq 1 8)
 do
-	ssh iad-mongo-usr10${i}-v240.ihr "sudo /sbin/sysctl -a" | sort > sysctl${i}.out
+	ssh iad-mongo-usr10${i}-v240.ihr "sudo /sbin/sysctl -a" | sort > sysctl_usr10${i}.out
+done
+for i in $(seq 1 4)
+do
+	ssh iad-mongo-fac10${i}-v240.ihr "sudo /sbin/sysctl -a" | sort > sysctl_fac10${i}.out
+done
+for i in $(seq 1 3)
+do
+	ssh iad-mongo-shared10${i}-v240.ihr "sudo /sbin/sysctl -a" | sort > sysctl_shared10${i}.out
 done
