e2ebdb31301099d6bcdcdcaaf810d2103adddbda
Irina Kaprizkina <irinakaprizkina@clearchannel.com>
2014-12-30 13:13:30 -0500

    .

--- bin/chef_compare_cookbook.pl
+++ bin/chef_compare_cookbook.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 
-
+my $debug=1;
 my @chef_servers=('prod','stage','qa_cloud'); 
 
 foreach my $var (@ARGV)
@@ -8,7 +8,19 @@ foreach my $var (@ARGV)
 	print "Checking cookbook $var ...\n";
 	foreach my $server (@chef_servers)
 	{
-		print `knife cookbook show $var -c ~/knife/knife.${server}.rb`;
+		my $out=`knife cookbook show $var -c ~/knife/knife.${server}.rb`;
+		
+		if ($? == 0)
+		{
+			my @version=split(/ /, $out);
+		}
+		else
+		{
+			if ($debug)
+			{
+				print "$out";
+			}
+		}
 	}
 }
 
