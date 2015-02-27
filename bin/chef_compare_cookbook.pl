4573a7e509be2f427ea5179aa3c77bbed9490744
Irina Kaprizkina <irinakaprizkina@clearchannel.com>
2015-02-27 12:52:11 -0500

    New chnages and new repo

--- bin/chef_compare_cookbook.pl
+++ bin/chef_compare_cookbook.pl
@@ -60,8 +60,12 @@ foreach my $var (@ARGV)
 	for my $i (0..$#chef_servers-1)
 	{ 
 		my $diffcmd="diff -r $dir/" . $chef_servers[$i] . " $dir/" . $chef_servers[$i+1];
-		print "$diffcmd\n\n";
-		print `$diffcmd`;
+		my $out = `$diffcmd`;
+		if ($out ne '')
+		{
+			print "$diffcmd\n\n";
+			print $out;
+		}
 	}
 
 }
